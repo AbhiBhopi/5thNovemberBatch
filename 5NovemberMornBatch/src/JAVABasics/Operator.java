339a5afd65566387405c98399f2cd0a6f5264428
AbhiBhopi <109980258+AbhiBhopi@users.noreply.github.com>
2023-04-07 07:53:43 +0530

    Update Operator.java

--- 5NovemberMornBatch/src/JAVABasics/Operator.java
+++ 5NovemberMornBatch/src/JAVABasics/Operator.java
@@ -6,7 +6,7 @@ public class Operator {
 	// && operator (And)
 		
 	System.out.println( (10>3)  &&  (12>7) );	//true
-	System.out.println( (10>4)  &&  (12>8) );	// unknown
+	System.out.println( (12>4)  &&  (10>8) );	// manger did some changes 
 	System.out.println( (10>3)  && (12<7) );  // false
 	
 	// or operator   ||
